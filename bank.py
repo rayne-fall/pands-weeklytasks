834e9cce81629bdd5ccfabafc85ead3766c2d9cb
Sylvia Chapman Kent <sylvia.kent.2.1@hotmail.com>
2024-02-04 17:01:50 +0000

    weekly task 2

--- bank.py
+++ bank.py
@@ -3,8 +3,8 @@
 # prompt the user to enter two money amounts in cent and output the sum of these two figures in euros and cents
 
 
-amount1 = input("Enter amount 1 in cent ")
-amount2 = input("Enter amount 2 in cent ")
+amount1 = int(input("Enter amount 1 in cent "))
+amount2 = int(input("Enter amount 2 in cent "))
 x = amount1
 y = amount2
 
@@ -12,7 +12,6 @@ answer = (x+y)
 
 print (answer)
 
-# output currently shows amount 1 and amount 2 beside each other instead of their sum e.g if amount 1 is 26 and amount 2 is 54, output shows 2654 instead of 80
-# script works fine if amounts are not variables so issue is likely because variables aren't implemented properly
+# output now gives correct figure in cent but isn't formatted into euro and cent yet
 
 
