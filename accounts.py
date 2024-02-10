f8124163725f843f557c4dd256bea534181aa5e7
Sylvia Chapman Kent <sylvia.kent.2.1@hotmail.com>
2024-02-10 16:30:29 +0000

    weekly task 3

--- accounts.py
+++ accounts.py
@@ -5,3 +5,6 @@
 account_number = input("Enter your 10 digit account number ")
 
 print(f"The account number is XXXXXX{account_number[6:]}")
+
+# References:
+# W3 Schools page on slicing strings https://www.w3schools.com/python/python_strings_slicing.asp
