897b78ce873ccd269f41bb30e7c28de9569703dc
Sylvia Chapman Kent <sylvia.kent.2.1@hotmail.com>
2024-02-28 20:53:49 +0000

    weekly task 6 (incomplete)

---
+++ squareroot.py
@@ -0,0 +1,9 @@
+# squareroot
+# Author: Sylvia Chapman Kent
+# reads in a positive number and outputs the approximate square root
+
+user_number = int(input("Enter a positive number: "))
+
+answer = user_number
+
+print (f"The approximate square root of {user_number} is {answer}")
